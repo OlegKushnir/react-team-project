f7ae0f22e8c0e7393badf0dbcd87b31e05789a09
OlegKushnir <108745741+OlegKushnir@users.noreply.github.com>
2022-11-29 10:24:59 +0000

    Deploying to gh-pages from @ OlegKushnir/react-team-project@33a51b5ff6e0033e6167d7bdeaede5fcab679c7b 🚀

--- static/js/642.5ae65f2f.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[642],{642:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"Statistics"})}}}]);
-//# sourceMappingURL=642.5ae65f2f.chunk.js.map
---
+++ static/js/642.fa71ef24.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[642],{7642:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"Statistics"})}}}]);
+//# sourceMappingURL=642.fa71ef24.chunk.js.map
